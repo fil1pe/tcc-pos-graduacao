90d012a871256ecc1db70a72c935da9033b5e839
Filipe Ramos <ik.ben.filipe@gmail.com>
2024-02-03 03:13:27 -0300

    Swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { EntityNotFoundExceptionFilter } from './filters/entity-not-found-exception.filter'
 import { ValidationPipe } from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalFilters(new EntityNotFoundExceptionFilter())
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
+  // OpenAPI:
+  const config = new DocumentBuilder().setTitle('API').build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
