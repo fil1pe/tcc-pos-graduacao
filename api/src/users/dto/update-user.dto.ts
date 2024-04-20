f135686f1aa4e06c8cf857ce6d6c5bcb890a4a9e
Filipe Ramos <ik.ben.filipe@gmail.com>
2024-04-20 02:05:22 -0300

    Corrige UpdateUserDto

--- api/src/users/dto/update-user.dto.ts
+++ api/src/users/dto/update-user.dto.ts
@@ -1,7 +1,6 @@
-import { OmitType } from '@nestjs/mapped-types'
 import { CreateUserDto } from './create-user.dto'
 import { encrypt } from 'src/helpers/encrypt.helper'
-import { ApiProperty, PartialType } from '@nestjs/swagger'
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger'
 import { IsNotEmpty } from 'class-validator'
 
 export class UpdateUserDto extends OmitType(PartialType(CreateUserDto), [
